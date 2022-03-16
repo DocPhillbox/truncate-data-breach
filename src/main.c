f1bb2d5989713702a2255e74fcc9d4c3c41aa36b
Andrey Bastien <71886221+DocPhillbox@users.noreply.github.com>
2022-03-16 15:54:43 +0100

    [main] Function documentation

--- src/main.c
+++ src/main.c
@@ -10,14 +10,33 @@ char user_out_filename[MAX_BUFFER] = "user-";
 char pass_out_filename[MAX_BUFFER] = "pass-";
 char seperator[MAX_BUFFER];
 
+/**
+ * @brief Write user into destination file
+ * 
+ * @param user Username to write in destination
+ * @param user_file User destination file
+ */
 void write_user(char *user, FILE *user_file) {
     fprintf(user_file, strcat(user, "\n"));
 }
 
+/**
+ * @brief Write password into destination file
+ * 
+ * @param pass Password to write in destination
+ * @param pass_file Password destination file
+ */
 void write_pass(char *pass, FILE *pass_file) {
     fprintf(pass_file, strcat(pass, "\n"));
 }
 
+/**
+ * @brief Split line and truncate username and password into two different files
+ * 
+ * @param line Line of the file to truncate
+ * @param user_file User destination file
+ * @param pass_file Password destination file
+ */
 void split_line_and_truncate(char *line, FILE *user_file, FILE *pass_file) {
     char *user = strtok(line, seperator);
     char *pass = strtok(strtok(NULL, ":;"), "\n");
@@ -25,11 +44,24 @@ void split_line_and_truncate(char *line, FILE *user_file, FILE *pass_file) {
     write_pass(pass, pass_file);
 }
 
+/**
+ * @brief Difference between start and end time in milliseconds
+ * 
+ * @param t0 Start time
+ * @param t1 End time
+ * @return float Difference in milliseconds
+ */
 float timedifference_msec(struct timeval t0, struct timeval t1)
 {
     return (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
 }
 
+/**
+ * @brief 
+ * 
+ * @return int 1 file not found
+ *             2 breach file not provided
+ */
 int main() {
     FILE *breach_file;
     char line[MAX_BUFFER];
@@ -38,11 +70,14 @@ int main() {
     float elapsed_time_in_milliseconds;
 
     printf("Enter the filename to split: ");
-    if (strlen(fgets(filename, MAX_BUFFER, stdin)) == 1)
-        return 0;
+    if (strlen(fgets(filename, MAX_BUFFER, stdin)) == 1) {
+        return 2;
+    }
+
     printf("Enter the seperator (default :;): ");
-    if (strlen(fgets(seperator, MAX_BUFFER, stdin)) == 1)
+    if (strlen(fgets(seperator, MAX_BUFFER, stdin)) == 1) {
         strcpy(seperator, ":;");
+    }
     
     filename[strcspn(filename, "\n")] = 0;
     seperator[strcspn(seperator, "\n")] = 0;
@@ -53,7 +88,7 @@ int main() {
     breach_file = fopen(filename, "r");
     if (breach_file == NULL) {
         printf("File not found...\n");
-        return 0;
+        return 1;
     }
 
     printf("Start truncat %s\n", filename);
